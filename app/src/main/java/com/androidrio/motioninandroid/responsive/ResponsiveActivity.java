a88f82d61a75431d4ae48a8637fe265fcd7921cc
AndroidRio <leonardo.engelet@gmail.com>
2016-06-04 21:28:12 -0300

    Reactive list.
    Next commits will be splitted properly.

--- app/src/main/java/com/androidrio/motioninandroid/responsive/ResponsiveActivity.java
+++ app/src/main/java/com/androidrio/motioninandroid/responsive/ResponsiveActivity.java
@@ -1,9 +1,10 @@
 package com.androidrio.motioninandroid.responsive;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.androidrio.motioninandroid.R;
+import com.androidrio.motioninandroid.responsive.react.ReactionListFragment;
 
 public class ResponsiveActivity extends AppCompatActivity {
 
@@ -11,5 +12,11 @@ public class ResponsiveActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_responsive);
+
+        if (savedInstanceState == null) {
+            ReactionListFragment fragment = ReactionListFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().replace(R.id.content_root, fragment, "reaction")
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/androidrio/motioninandroid/responsive/react/ReactionListAdapter.java
@@ -0,0 +1,42 @@
+package com.androidrio.motioninandroid.responsive.react;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.androidrio.motioninandroid.R;
+
+/**
+ * Created by AndroidRio on 13/04/2016.
+ */
+public class ReactionListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    public ReactionListAdapter() {
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_reaction_list, parent, false);
+        ReactionItemViewHolder holder = new ReactionItemViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    }
+
+    @Override
+    public int getItemCount() {
+        return 20;
+    }
+
+    public class ReactionItemViewHolder extends RecyclerView.ViewHolder {
+
+        public ReactionItemViewHolder(View itemView) {
+            super(itemView);
+        }
+
+    }
+
+}
