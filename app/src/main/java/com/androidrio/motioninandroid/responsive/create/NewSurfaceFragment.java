90ad768422de8e75eaf7005aa26360e22a28e56d
AndroidRio <leonardo.engelet@gmail.com>
2016-06-04 23:07:41 -0300

    Adds the new surface animation.
    It still needs to be improved since the expand animation is linear. In the material specs it shouldn't be that linear since material doesn't move like that in the real world.

--- app/src/main/java/com/androidrio/motioninandroid/responsive/create/NewSurfaceFragment.java
+++ app/src/main/java/com/androidrio/motioninandroid/responsive/create/NewSurfaceFragment.java
@@ -1,17 +1,28 @@
 package com.androidrio.motioninandroid.responsive.create;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 
 import com.androidrio.motioninandroid.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class NewSurfaceFragment extends Fragment {
+    @Bind(R.id.fragment_new_surface_toggle)
+    ImageButton mToogle;
+    @Bind(R.id.fragment_new_surface_card)
+    CardView mCard;
 
     public NewSurfaceFragment() {
-        // Required empty public constructor
     }
 
     public static NewSurfaceFragment newInstance() {
@@ -28,6 +39,43 @@ public class NewSurfaceFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_surface, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_new_surface, container, false);
+        ButterKnife.bind(this, rootView);
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mToogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCard.getVisibility() == View.GONE) {
+                    mCard.setVisibility(View.VISIBLE);
+                    Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_new_surface_expand);
+                    mCard.startAnimation(animation);
+                }else{
+                    Animation animation = AnimationUtils.loadAnimation(getActivity(), R.anim.anim_new_surface_collapse);
+                    animation.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            mCard.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+                    mCard.startAnimation(animation);
+                }
+            }
+        });
     }
 }
