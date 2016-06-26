02a3201564c2c7b5530b5be7308b717736fb2984
AndroidRio <leonardo.engelet@gmail.com>
2016-06-25 22:52:15 -0300

    Updates the enter transition from the react list.

--- app/src/main/java/com/androidrio/motioninandroid/motion/responsive/react/ReactionListFragment.java
+++ app/src/main/java/com/androidrio/motioninandroid/motion/responsive/react/ReactionListFragment.java
@@ -5,12 +5,14 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Transition;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.androidrio.motioninandroid.R;
 import com.androidrio.motioninandroid.widget.DividerItemDecoration;
+import com.androidrio.motioninandroid.widget.TransitionListenerAdapter;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -19,6 +21,8 @@ public class ReactionListFragment extends Fragment {
 
     @Bind(R.id.fragment_reaction_list)
     RecyclerView mReactionList;
+
+    private ReactionListAdapter mAdapter;
 
     public ReactionListFragment() {
     }
@@ -45,12 +49,19 @@ public class ReactionListFragment extends Fragment {
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         DividerItemDecoration itemDecoration = new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST, 8);
         mReactionList.addItemDecoration(itemDecoration);
         LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
         mReactionList.setLayoutManager(layoutManager);
-        ReactionListAdapter adapter = new ReactionListAdapter();
-        mReactionList.setAdapter(adapter);
+        mAdapter = new ReactionListAdapter();
+        mReactionList.setAdapter(mAdapter);
+        getActivity().getWindow().getSharedElementEnterTransition().addListener(new TransitionListenerAdapter(){
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                mAdapter.animateContent();
+            }
+        });
+
+        getActivity().setTitle("React List");
     }
 }
