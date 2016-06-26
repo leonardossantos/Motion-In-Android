4b3dd523543155536d10200bbc53b90b1b3a095b
AndroidRio <leonardo.engelet@gmail.com>
2016-06-25 23:49:53 -0300

    Adds animation listener adapter.

---
+++ app/src/main/java/com/androidrio/motioninandroid/widget/AnimationListenerAdapter.java
@@ -0,0 +1,23 @@
+package com.androidrio.motioninandroid.widget;
+
+import android.view.animation.Animation;
+
+/**
+ * Created by AndroidRio on 25/06/2016.
+ */
+public class AnimationListenerAdapter implements Animation.AnimationListener {
+    @Override
+    public void onAnimationStart(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+}
